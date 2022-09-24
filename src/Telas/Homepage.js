0976e9314986aee7d043147982aebbe9e75b2201
Elizabeth <eliza8@hotmail.com.br>
2022-09-24 20:02:21 -0300

    atualizando tela login

--- src/Telas/Homepage.js
+++ src/Telas/Homepage.js
@@ -1,8 +1,17 @@
+import * as React from 'react';
+
 import {Link, TextField} from "@mui/material";
 import Button from "@mui/material/Button";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import {useNavigate } from "react-router-dom";
-import React from "react";
+
+import imagem_trabalho from "../imagem_trabalho.png"
+import arte_novo from "../arte_novo.png"
+import logo from "../logo.png"
+
+import InputAdornment from '@mui/material/InputAdornment';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import LockIcon from '@mui/icons-material/Lock';
 
 function Homepage() {
     const navigate = useNavigate();
@@ -30,66 +39,92 @@ function Homepage() {
             setErrors("por favor, preencha todos os campos")
         }
     }
+
     return (
-        <div className="split left">
-        <div className="centered">
-            <h2 className="preto"> F </h2>
-            <h2 className="preto-claro"> R </h2>
-            <h2 className="cinza-escuro"> U </h2>
-            <h2 className="cinza"> B </h2>
+        <><div className="split left">
+            <div className="centered">
+                <img src={logo} className="logo2"/>
+                <img src={arte_novo} className="arte" />
+                <div className="texto-padrao">
+                    <h4 style={{marginBottom: "25px"}}>Bem-vindo de volta!</h4>
+                </div>
 
-            <div className="texto-padrao">
-                <h3> Inicie sua sessao. </h3>
+                <div className="texto-padrao" style={{ textAlign: "left" }}>
+                    <h5 style={{ marginBottom: "10px" }}>Iniciar sessão:</h5>
+                </div>
+
+                <TextField className="input"
+                    margin="normal"
+                    size="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    variant={"filled"}
+                    placeholder="email@email.com"
+                    value={email}
+                    error={errors}
+                    helperText={errors}
+                    onChange={handleChangeEmail}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <AccountCircle />
+                            </InputAdornment>
+                        ),
+                    }} />
+
+                <TextField className="input"
+                    marginTop="15px"
+                    required
+                    fullWidth
+                    name="senha"
+                    label="Senha"
+                    type="senha"
+                    id="senha"
+                    variant={"filled"}
+                    placeholder="******"
+                    value={senha}
+                    onChange={handleChangeSenha}
+                    error={errors}
+                    helperText={errors}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <LockIcon />
+                            </InputAdornment>
+                        ),
+                    }} />
+
+                <Button
+                    style={{margin: "10px"}}
+                    background-color="#ffffff"
+                    variant="contained"
+                    size={"large"}
+                    endIcon={<ArrowForwardIcon />}
+                    onClick={logar}>
+
+                    Entrar</Button>
+
+                <div className="texto-padrao mini">
+                    <h5 style={{ marginTop: "8px"}}>Não possui cadastro? <Link href={"/register"} onClick={registrar}><a>Clique aqui</a></Link></h5>
+                </div>
+
             </div>
-
-            <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                variant={"filled"}
-                value={email}
-                error={errors}
-                helperText={errors}
-                onChange={handleChangeEmail}
-            />
-            <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="senha"
-                label="Senha"
-                type="senha"
-                id="senha"
-                variant={"filled"}
-                value={senha}
-                onChange={handleChangeSenha}
-                error={errors}
-                helperText={errors}
-            />
-
-            <Button
-                variant="contained"
-                size={"large"}
-                endIcon={<ArrowForwardIcon/>}
-                onClick={logar}
-            >
-                Entrar</Button>
-
-            <div className="texto-padrao mini">
-                <h3>Não possui cadastro? </h3>
-                <Link href={"/register"} onClick={registrar}>
-                    Clique aqui
-                </Link>
-            </div>
-
         </div>
-    </div>
+
+        <div className="split right">
+        <div className="centered">
+            <img src={imagem_trabalho} className="image" />
+        </div>
+        </div></>
+        
+    
     );
+
 }
 
 export default Homepage;
