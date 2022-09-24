0577292b61d440e7f523e3c1c88754c01d7832c6
Jordana Tomio <dannatomio@gmail.com>
2022-09-24 17:54:47 -0300

    Refactor

--- src/Login.js
+++ src/Login.js
@@ -27,6 +27,7 @@ export default function Login() {
     };
     const [tipo, setTipo] = React.useState('');
     const [comentario, setComentario] = React.useState('');
+    const [assunto, setAssunto] = useState('');
     const [email, setEmail] = React.useState('');
     const [errors, setErrors] = React.useState('');
     const [dadosUsuario, setDadosUsuario] = useState(jsonData);
@@ -39,26 +40,37 @@ export default function Login() {
         setEmail(event.target.value);
     };
 
+    const handleChangeAssunto = (event) => {
+        setAssunto(event.target.value);
+    };
+
     const handleChangeComentario = (event) => {
         setComentario(event.target.value);
     };
 
     const objetoTabela = {
         tipo,
-        comentario,
+        assunto
     };
 
-    const tableRows = dadosUsuario.map((info) => {
+    const colunasTabela = dadosUsuario.map((info) => {
         return (
             <tr>
                 <td>{info.tipo}</td>
-                <td>{info.comentario}</td>
+                <td>{info.assunto}</td>
             </tr>
         );
     });
 
-    const addRows = () => {
-        if (email.length < 4 || tipo.length < 4 || comentario.length < 4) {
+    const resetar = () => {
+        setTipo('');
+        setComentario('');
+        setEmail('')
+        setAssunto('')
+    }
+
+    const inserirTabela = () => {
+        if (assunto.length < 4 || email.length < 4 || tipo.length < 4 || comentario.length < 4) {
             setErrors("por favor, preencha todos os campos")
         } else {
             setErrors('')
@@ -68,7 +80,8 @@ export default function Login() {
 
             setTipo('');
             setComentario('');
-            setEmail('')
+            setEmail('');
+            setAssunto('');
         }
     };
 
@@ -107,8 +120,8 @@ export default function Login() {
                     ))}
                 </Toolbar>
 
-                <div className="card">
-                    <Card variant="outlined" sx={{minWidth: 40}} className="card2">
+                <div className="cards">
+                    <Card variant="outlined" sx={{minWidth: 40}} className="card-conteudo">
                         <CardContent>
                             <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
                                 BOAS VINDAS
@@ -119,7 +132,7 @@ export default function Login() {
                         </CardContent>
                     </Card>
 
-                    <Card variant="outlined" sx={{minWidth: 50}} className="card2">
+                    <Card variant="outlined" sx={{minWidth: 50}} className="card-conteudo">
                         <CardContent>
                             <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
                                 FORMULARIO
@@ -128,7 +141,7 @@ export default function Login() {
 
                             <div className="card-texto">
                                 <FormControl>
-                                    <InputLabel id="tipo-comentario-label">Tipo Comentario</InputLabel>
+                                    <InputLabel id="tipo-comentario-label">Tipo Comentario*</InputLabel>
                                     <Select
                                         labelId="tipo-comentario-label"
                                         id="tipo-comentario"
@@ -137,7 +150,10 @@ export default function Login() {
                                         error={errors}
                                         helperText={errors}
                                         onChange={handleChangeTipo}
+                                        value={tipo}
+                                        required
                                     >
+                                        <MenuItem value=""><em>None</em></MenuItem>
                                         <MenuItem value={'critica'}>Critica</MenuItem>
                                         <MenuItem value={'sugestao'}>Sugestao</MenuItem>
                                         <MenuItem value={'elogio'}>Elogio</MenuItem>
@@ -150,6 +166,17 @@ export default function Login() {
                                                value={email}
                                                error={errors}
                                                helperText={errors}
+                                               required
+                                    />
+
+                                    <TextField variant="outlined"
+                                               label={"Assunto"}
+                                               className="espacamento"
+                                               onChange={handleChangeAssunto}
+                                               value={assunto}
+                                               error={errors}
+                                               helperText={errors}
+                                               required
                                     />
 
                                     <TextField variant="outlined"
@@ -161,35 +188,42 @@ export default function Login() {
                                                value={comentario}
                                                error={errors}
                                                helperText={errors}
+                                               required
                                     />
 
                                 </FormControl>
                             </div>
                         </CardContent>
                         <CardActions>
-                            <Button size="small" onClick={addRows}>Enviar</Button>
+                            <Button size="small" variant="contained" onClick={inserirTabela}>Enviar</Button>
+                            <Button size="small" variant="contained" onClick={resetar}>Limpar</Button>
                         </CardActions>
                     </Card>
 
-                    <Card variant="outlined" sx={{minWidth: 40}} className="card2">
+                    <Card variant="outlined" sx={{minWidth: 40}} className="card-conteudo">
                         <CardContent>
-                            <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+
+                            <Typography sx={{fontSize: 14}} color="text.secondary" >
                                 TABELA
                             </Typography>
 
+                            <a>Aqui você pode acompanhar seu hisórico de comentários.</a>
+                            <br></br>
+
                             <div>
-                                <table className="table table-stripped">
+                                <table>
                                     <thead>
                                     <tr>
                                         <th>Tipo</th>
-                                        <th>Comentario</th>
+                                        <th>Assunto</th>
                                     </tr>
                                     </thead>
-                                    <tbody>{tableRows}</tbody>
+                                    <tbody>{colunasTabela}</tbody>
                                 </table>
 
                                 <br/>
                             </div>
+
                         </CardContent>
                     </Card>
                 </div>
