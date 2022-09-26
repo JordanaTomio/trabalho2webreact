c4b80ead7091868c3b29ff86bf5fb825f4f9fcc1
Elizabeth <eliza8@hotmail.com.br>
2022-09-26 02:23:29 -0300

    adicionado barra de pesquisa no cabecalho

--- src/Telas/Cabecalho.js
+++ src/Telas/Cabecalho.js
@@ -8,7 +8,46 @@ import {Link, useNavigate} from "react-router-dom";
 import React from "react";
 import {Link as Link2} from "@mui/material";
 
+import { styled, alpha } from '@mui/material/styles';
+
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
 function Cabecalho() {
+
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,   
+        marginLeft: 100,
+        width: '20%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing(1),
+          width: 'auto',
+        },
+      }));
+      
+      const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }));
+      
+      const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        '& .MuiInputBase-input': {
+          padding: theme.spacing(1, 1, 1, 0),
+          paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+          width: '100%',
+          [theme.breakpoints.up('sm')]: {
+            width: '18ch',
+          },
+        },
+      }));      
+
     const [anchorEl, setAnchorEl] = React.useState(null);
 
     const handleClick = (index, event) => {
@@ -67,8 +106,24 @@ function Cabecalho() {
                     </div>
                 ))}
             </Toolbar>
+
+            <div className="barra-pesquisa">
+            <Toolbar>
+                <Search sx={{background: 'white'}}>
+                    <SearchIconWrapper>
+                        <SearchIcon />
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                        placeholder="Pesquisar página…"
+                        inputProps={{ 'aria-label': 'search' }}
+                    />
+                </Search>
+            </Toolbar>
+            </div>
+            
         </div>
 )
+
 }
 
 export default Cabecalho;
